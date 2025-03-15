7ef1d992eec095bab58d06ac3afc3a9fe50a4c97
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-15 10:13:51 +0630

    -

--- GraphQL/BlogMutation.cs
+++ GraphQL/BlogMutation.cs
@@ -2,36 +2,35 @@
 using csharp_graphql.Repositories;
 using HotChocolate.Subscriptions;
 
-namespace csharp_graphql.GraphQL
+namespace csharp_graphql.GraphQL;
+
+public class BlogMutation
 {
-    public class BlogMutation
+    /*
+     mutation {
+      addBlog(blog: {
+        blogId: 4,
+        blogTitle: "Blog Title 4",
+        blogAuthor: "Blog Author 4",
+        blogContent: "Blog Content 4",
+        isDeleted: false
+      }) {
+        blogId
+        blogTitle
+        blogAuthor
+        blogContent
+        isDeleted
+      }
+    }
+     */
+    public async Task<BlogModel> AddBlog([Service] IBlogRepository blogRepository,
+        [Service] ITopicEventSender eventSender,
+        BlogModel blog)
     {
-        /*
-         mutation {
-          addBlog(blog: {
-            blogId: 4,
-            blogTitle: "Blog Title 4",
-            blogAuthor: "Blog Author 4",
-            blogContent: "Blog Content 4",
-            isDeleted: false
-          }) {
-            blogId
-            blogTitle
-            blogAuthor
-            blogContent
-            isDeleted
-          }
-        }
-         */
-        public async Task<BlogModel> AddBlog([Service] IBlogRepository blogRepository,
-            [Service] ITopicEventSender eventSender,
-            BlogModel blog)
-        {
-            blogRepository.AddBlog(blog);
+        blogRepository.AddBlog(blog);
 
-            await eventSender.SendAsync("BlogAdded", blog);
+        await eventSender.SendAsync("BlogAdded", blog);
 
-            return blog;
-        }
+        return blog;
     }
 }
