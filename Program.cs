51eb01f496b648fd0367d50b8017913c844f5ddd
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-15 10:15:20 +0630

    -

--- Program.cs
+++ Program.cs
@@ -1,49 +1,48 @@
 using csharp_graphql.GraphQL;
 using csharp_graphql.Repositories;
 
-namespace csharp_graphql
+namespace csharp_graphql;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers().AddJsonOptions(opt =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-            // Add services to the container.
+        builder.Services.AddSingleton<IBlogRepository, BlogRepository>();
+        builder.Services.AddGraphQLServer()
+            .AddQueryType<BlogQuery>()
+            .AddMutationType<BlogMutation>()
+            .AddSubscriptionType<BlogSubscription>()
+            .AddInMemorySubscriptions();
 
-            builder.Services.AddControllers().AddJsonOptions(opt =>
-            {
-                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-            });
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        var app = builder.Build();
+        app.MapGraphQL("/");
 
-            builder.Services.AddSingleton<IBlogRepository, BlogRepository>();
-            builder.Services.AddGraphQLServer()
-                .AddQueryType<BlogQuery>()
-                .AddMutationType<BlogMutation>()
-                .AddSubscriptionType<BlogSubscription>()
-                .AddInMemorySubscriptions();
-
-            var app = builder.Build();
-            app.MapGraphQL("/");
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
